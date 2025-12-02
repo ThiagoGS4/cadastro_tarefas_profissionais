f60457983405d6bfb2ed588839e269042218febf
Thiago Garcia Santos <thigasan@gmail.com>
2025-12-01 23:18:58 -0300

    funfano

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,11 +6,15 @@ class Home extends StatefulWidget {
   @override
   State<Home> createState() => HomeState();
 }
+
 class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Tarefas'),
-          centerTitle: true,
-        ),);
-}}
+      appBar: AppBar(title: const Text('Tarefas'), centerTitle: true),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [Row()],
+      ),
+    );
+  }
+}
